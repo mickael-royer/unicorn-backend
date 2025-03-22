53d96d83060d3d6b1d20a74efce8b1579b639113
royerm <royerm@gmail.com>
2025-03-22 13:29:04 +0100

    Troubleshooting CosmosDB Endpoint

--- python-service/app/config.py
+++ python-service/app/config.py
@@ -9,7 +9,7 @@ MODEL_NAME = "gemini-2.0-flash"  # Or "gemini-pro-vision"
 GEMINI_EMBEDDING_MODEL = "models/embedding-001"
 
 # Cosmos DB Configuration
-COSMOS_ENDPOINT = "https://account-cosmodb-westeurope.documents.azure.com:443/"
+COSMOS_ENDPOINT = os.getenv("COSMOS_ENDPOINT")
 COSMOS_KEY = os.getenv("COSMOS_KEY")
 DATABASE_NAME = "filesDB"
 CONTAINER_NAME = "c4"
